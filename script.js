6b7fd9b60b91dfffe82e28bfb07524c4172e8aed
pratik-dev18 <webdeveloper191801@gmail.com>
2026-04-11 14:29:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// LOADER
+window.addEventListener("load", () => {
+  document.getElementById("loader").style.display = "none";
+});
+
+// IMAGE OPEN
+function openImage(src) {
+  window.open(src, "_blank");
+}
